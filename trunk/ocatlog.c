9aeae174664c9a63fc477c1517a426dd1d84dd07
eagle <eagle@58e1ccc2-750e-0410-8d0d-f93ca75ab447>
2008-02-08 23:31:47 +0000

    thread startup improved local control socket implemented with some commands

--- trunk/ocatlog.c
+++ trunk/ocatlog.c
@@ -17,24 +17,24 @@ static const char *flty_[] = {"info", "notice", "error", "fatal", "debug"};
 
 void log_msg(int lf, const char *fmt, ...)
 {
-   unsigned tid;
    struct tm *tm;
    time_t t;
    FILE *out = stderr;
    char timestr[32] = "";
    va_list ap;
+   const OcatThread_t *th = get_thread();
 
    if (debug_level_ < lf || lf < 0)
       return;
 
+   //th = get_thread();
    t = time(NULL);
    tm = localtime(&t);
    if (tm)
       strftime(timestr, 32, "%c", tm);
-   tid = (unsigned) pthread_self();
 
    pthread_mutex_lock(&log_mutex_);
-   fprintf(out, "%s [%08x] %6s ", timestr, tid, flty_[lf]);
+   fprintf(out, "%s [%d:%-*s] %6s ", timestr, th->id, THREAD_NAME_LEN - 1, th->name, flty_[lf]);
 
    va_start(ap, fmt);
    vfprintf(out, fmt, ap);
